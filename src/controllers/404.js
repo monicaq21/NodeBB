1aed939a477fd49a0dd68224b9673c0cd62f7f8f
Monica Qiu <m1102778586@gmail.com>
2023-09-04 13:44:18 -0400

    recompile javascript file

--- src/controllers/404.js
+++ src/controllers/404.js
@@ -8,19 +8,22 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
-const nconf = require('nconf');
-const winston = require('winston');
-const validator = require('validator');
-const meta = require('../meta');
-const plugins = require('../plugins');
-const middleware = require('../middleware');
-const helpers = require('../middleware/helpers');
+const nconf_1 = __importDefault(require("nconf"));
+const winston_1 = __importDefault(require("winston"));
+const validator_1 = __importDefault(require("validator"));
+const meta_1 = __importDefault(require("../meta"));
+const plugins_1 = __importDefault(require("../plugins"));
+const middleware_1 = __importDefault(require("../middleware"));
+const helpers_1 = __importDefault(require("../middleware/helpers"));
 exports.handle404 = function handle404(req, res) {
-    const relativePath = nconf.get('relative_path');
+    const relativePath = nconf_1.default.get('relative_path');
     const isClientScript = new RegExp(`^${relativePath}\\/assets\\/src\\/.+\\.js(\\?v=\\w+)?$`);
-    if (plugins.hooks.hasListeners('action:meta.override404')) {
-        return plugins.hooks.fire('action:meta.override404', {
+    if (plugins_1.default.hooks.hasListeners('action:meta.override404')) {
+        return plugins_1.default.hooks.fire('action:meta.override404', {
             req: req,
             res: res,
             error: {},
@@ -32,14 +35,14 @@ exports.handle404 = function handle404(req, res) {
     else if (!res.locals.isAPI && (req.path.startsWith(`${relativePath}/assets/uploads`) ||
         (req.get('accept') && !req.get('accept').includes('text/html')) ||
         req.path === '/favicon.ico')) {
-        meta.errors.log404(req.path || '');
+        meta_1.default.errors.log404(req.path || '');
         res.sendStatus(404);
     }
     else if (req.accepts('html')) {
         if (process.env.NODE_ENV === 'development') {
-            winston.warn(`Route requested but not found: ${req.url}`);
+            winston_1.default.warn(`Route requested but not found: ${req.url}`);
         }
-        meta.errors.log404(req.path.replace(/^\/api/, '') || '');
+        meta_1.default.errors.log404(req.path.replace(/^\/api/, '') || '');
         exports.send404(req, res);
     }
     else {
@@ -52,16 +55,16 @@ exports.send404 = function (req, res) {
         const path = String(req.path || '');
         if (res.locals.isAPI) {
             return res.json({
-                path: validator.escape(path.replace(/^\/api/, '')),
+                path: validator_1.default.escape(path.replace(/^\/api/, '')),
                 title: '[[global:404.title]]',
-                bodyClass: helpers.buildBodyClass(req, res),
+                bodyClass: helpers_1.default.buildBodyClass(req, res),
             });
         }
-        yield middleware.buildHeaderAsync(req, res);
+        yield middleware_1.default.buildHeaderAsync(req, res);
         yield res.render('404', {
-            path: validator.escape(path),
+            path: validator_1.default.escape(path),
             title: '[[global:404.title]]',
-            bodyClass: helpers.buildBodyClass(req, res),
+            bodyClass: helpers_1.default.buildBodyClass(req, res),
         });
     });
 };
